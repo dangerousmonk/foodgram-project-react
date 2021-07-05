ac99981e1683bb541e8ecdf0dab2d46af33e2e8e
Avilov Oleg <dangerousmonk@yandex.ru>
2021-07-05 22:34:41 +0300

    Set fname, lname as required fields

--- foodgram/users/models.py
+++ foodgram/users/models.py
@@ -5,11 +5,24 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     email = models.EmailField(
+        _('Email адрес'),
         unique=True,
         blank=False,
         null=False,
-        verbose_name=_('Email адрес'),
     )
+    first_name = models.CharField(
+        _('Имя'),
+        max_length=150,
+        blank=False,
+        null=False,
+    )
+    last_name = models.CharField(
+        _('Фамилия'),
+        max_length=150,
+        blank=False,
+        null=False,
+    )
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
     class Meta:
         ordering = ('username',)
@@ -17,4 +30,7 @@ class User(AbstractUser):
         verbose_name_plural = _('Пользователи')
 
     def __str__(self):
-        return self.username
+        return self.get_full_name()
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
