3ca770669e06fe82e376e395573025925e1a452b
Avilov Oleg <dangerousmonk@yandex.ru>
2021-07-21 19:39:20 +0300

    Rework structure

---
+++ foodgram/settings-dev.py
@@ -0,0 +1,12 @@
+from .settings import *  # noqa
+from .settings import env
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+SECRET_KEY = env.str('SECRET_KEY')
