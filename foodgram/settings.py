611540c15ce35b0c88e05a283b4c954b6d8cf0bd
Avilov Oleg <dangerousmonk@yandex.ru>
2021-07-22 23:41:23 +0300

    Fix test run

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
